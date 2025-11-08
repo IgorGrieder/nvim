f6b4a55363ca940c497c1995d8de3d72d54ece26
Igor Grieder Melo <igorgrieder@gmail.com>
2025-11-08 16:00:20 -0300

    new dap keymaps

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -26,43 +26,17 @@ return {
         dapui.close()
       end
 
-      vim.keymap.set("n", "<Leader>dt", ":DapUiToggle<CR>", {})
-      vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
-      vim.keymap.set("n", "<Leader>dc", dap.continue, {})
-      vim.keymap.set("n", "<Leader>dr", ":lua require('dapui').open({reset = true})<CR>", {})
+      vim.keymap.set("n", "<Leader>dk", dap.terminate, { desc = "Terminate Session" })
+      vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+      vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "Continue" })
+      vim.keymap.set("n", "<Leader>di", dap.step_into, { desc = "DAP Step Into" })
+      vim.keymap.set("n", "<Leader>dn", dap.run_to_cursor, { desc = "DAP Run to cursor" })
+      vim.keymap.set("n", "<Leader>dr", ":lua require('dapui').open({reset = true})<CR>", { desc = "Start DAP" })
 
       vim.fn.sign_define(
         "DapBreakpoint",
         { text = "⏺", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
       )
-    end,
-  },
-  {
-    "mfussenegger/nvim-dap-python",
-    ft = "python",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-    },
-    config = function()
-      local dap, dapui = require("dap"), require("dapui")
-
-      require("dapui").setup()
-
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
-
-      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python3"
-      require("dap-python").setup(path)
     end,
   },
 }
