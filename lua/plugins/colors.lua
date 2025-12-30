93d4b74158f13c2a917be2056b27afd4a7a86b46
Igor Grieder Melo <igorgrieder@gmail.com>
2025-12-29 22:39:20 -0300

    new colors

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,138 +1,100 @@
-function ColorMyPencils(color)
-  --[[ color = color or "kanagawa" ]]
-  --[[ color = color or "tokyonight" ]]
-  -- color = "github-nvim-theme"
-  color = color or "rose-pine"
-  vim.cmd.colorscheme(color)
+-- 1. Define the function at the top so every plugin can see it
+local function ColorMyPencils(color)
+  -- Default to tokyonight if nothing is passed
+  color = color or "tokyonight"
 
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+  -- Try to load the colorscheme safely
+  local ok, _ = pcall(vim.cmd.colorscheme, color)
+  if not ok then
+    -- If the theme fails to load (maybe not installed yet), don't crash
+    return
+  end
 
-  vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
+  -- Apply your transparency overrides consistently
+  local hl_groups = { "Normal", "NormalFloat", "LineNr", "CursorLine" }
+  for _, group in ipairs(hl_groups) do
+    vim.api.nvim_set_hl(0, group, { bg = "none" })
+  end
 end
 
 return {
+  -- Essential for loading colorschemes correctly in lazy.nvim
+  { "erikbackman/brightburn.vim" },
 
   {
-    "erikbackman/brightburn.vim",
-  },
-  {
     "rebelot/kanagawa.nvim",
-    compile = false, -- enable compiling the colorscheme
-    undercurl = true, -- enable undercurls
-    commentStyle = { italic = true },
-    functionStyle = {},
-    keywordStyle = { italic = true },
-    statementStyle = { bold = true },
-    typeStyle = {},
-    transparent = false, -- do not set background color
-    dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-    terminalColors = true, -- define vim.g.terminal_color_{0,17}
-    colors = { -- add/modify theme and palette colors
-      palette = {},
-      theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-    },
-    overrides = function(colors) -- add/modify highlights
-      return {}
-    end,
-    theme = "lotus", -- Load "wave" theme
-    background = { -- map the value of 'background' option to a theme
-      dark = "lotus", -- try "dragon" !
-      light = "lotus",
+    opts = {
+      compile = false,
+      undercurl = true,
+      commentStyle = { italic = true },
+      keywordStyle = { italic = true },
+      statementStyle = { bold = true },
+      transparent = false,
+      theme = "lotus",
+      background = { dark = "lotus", light = "lotus" },
     },
   },
+
   {
     "folke/tokyonight.nvim",
     lazy = false,
-    opts = {},
+    priority = 1000,
     config = function()
-      ColorMyPencils()
+      require("tokyonight").setup({
+        style = "night",
+        transparent = false,
+        styles = {
+          comments = { italic = false },
+          keywords = { italic = false },
+          sidebars = "dark",
+          floats = "dark",
+        },
+      })
+
+      -- ACTIVATE THE THEME HERE
+      ColorMyPencils("tokyonight")
     end,
   },
+
   {
     "ellisonleao/gruvbox.nvim",
     name = "gruvbox",
     config = function()
       require("gruvbox").setup({
-        terminal_colors = true, -- add neovim terminal colors
+        terminal_colors = true,
         undercurl = true,
-        underline = false,
         bold = true,
-        italic = {
-          strings = false,
-          emphasis = false,
-          comments = false,
-          operators = false,
-          folds = false,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = "hard", -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
+        contrast = "hard",
       })
     end,
   },
-  {
-    "folke/tokyonight.nvim",
-    config = function()
-      require("tokyonight").setup({
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-        transparent = false, -- Enable this to disable setting the background color
-        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-        styles = {
-          -- Style to be applied to different syntax groups
-          -- Value is any valid attr-list value for `:help nvim_set_hl`
-          comments = { italic = false },
-          keywords = { italic = false },
-          -- Background styles. Can be "dark", "transparent" or "normal"
-          sidebars = "dark", -- style for sidebars, see below
-          floats = "dark", -- style for floating windows
-        },
-      })
-    end,
-  },
+
   {
     "projekt0n/github-nvim-theme",
     name = "github-theme",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      require("github-theme").setup({
-        -- ...
-      })
-
-      -- vim.cmd("colorscheme github_dark")
+      require("github-theme").setup({})
+      -- To use this theme, move ColorMyPencils("github_dark") here
     end,
   },
+
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
+    "catppuccin/nvim",
+    name = "catppuccin",
     config = function()
-      require("rose-pine").setup({
-        transparent = false, -- Enable this to disable setting the background color
-        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-        styles = {
-          -- Style to be applied to different syntax groups
-          -- Value is any valid attr-list value for `:help nvim_set_hl`
-          comments = { italic = false },
-          keywords = { italic = false },
-          -- Background styles. Can be "dark", "transparent" or "normal"
-          sidebars = "dark", -- style for sidebars, see below
-          floats = "dark", -- style for floating windows
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = false,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
         },
       })
 
-      ColorMyPencils()
+      -- If you want to switch to catppuccin, uncomment the line below
+      -- and comment out the one in tokyonight
+      -- ColorMyPencils("catppuccin")
     end,
   },
 }
